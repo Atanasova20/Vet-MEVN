6162dbe810f37b8353f1ee598f4c52e1bb555d3c
Vanya Atanasova <Vanyaatanasova1111@gmail.com>
2024-09-08 20:23:45 +0300

    first commit: initial build

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "home": "url(./public/img/cocker.jpg)"
+      }
+    },
   },
   plugins: [],
 }
-
